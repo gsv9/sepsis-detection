469e1ff8c8873bd3f2b8439b24f4224d6a52dea7
gsv9 <gsaivishesh@gmail.com>
2026-03-08 11:22:15 +0530

    Refactor layer1_mac module to use parallel weights

--- rtl/layer1_mac.v
+++ rtl/layer1_mac.v
@@ -1,4 +1,4 @@
-module layer1_mac(
+module layer1_parallel_mac(
 
 input clk,
 input rst,
@@ -10,63 +10,49 @@ input signed [7:0] feature2,
 input signed [7:0] feature3,
 input signed [7:0] feature4,
 
-input signed [7:0] weight,
+input signed [7:0] w0,
+input signed [7:0] w1,
+input signed [7:0] w2,
+input signed [7:0] w3,
+input signed [7:0] w4,
+
 input signed [15:0] bias,
 
 output reg signed [31:0] neuron_out,
-output reg [2:0] neuron_index,
 output reg valid_out
 
 );
 
-reg signed [31:0] accumulator;
-reg [2:0] feature_count;
-reg [2:0] neuron_count;
+wire signed [15:0] m0;
+wire signed [15:0] m1;
+wire signed [15:0] m2;
+wire signed [15:0] m3;
+wire signed [15:0] m4;
 
-wire signed [7:0] feature_mux;
+assign m0 = feature0 * w0;
+assign m1 = feature1 * w1;
+assign m2 = feature2 * w2;
+assign m3 = feature3 * w3;
+assign m4 = feature4 * w4;
 
-assign feature_mux =
-    (feature_count==0) ? feature0 :
-    (feature_count==1) ? feature1 :
-    (feature_count==2) ? feature2 :
-    (feature_count==3) ? feature3 :
-                         feature4;
+wire signed [31:0] sum;
+
+assign sum = m0 + m1 + m2 + m3 + m4;
 
 always @(posedge clk or posedge rst)
 begin
-
     if(rst)
     begin
-        accumulator <= 0;
-        feature_count <= 0;
-        neuron_count <= 0;
+        neuron_out <= 0;
         valid_out <= 0;
     end
-
     else if(valid_in)
     begin
-
-        accumulator <= accumulator + feature_mux * weight;
-
-        if(feature_count == 4)
-        begin
-            neuron_out <= accumulator + bias;
-            neuron_index <= neuron_count;
-            valid_out <= 1;
-
-            accumulator <= 0;
-            feature_count <= 0;
-            neuron_count <= neuron_count + 1;
-        end
-
-        else
-        begin
-            feature_count <= feature_count + 1;
-            valid_out <= 0;
-        end
-
+        neuron_out <= sum + bias;
+        valid_out <= 1;
     end
-
+    else 
+    valid_out<=0;
 end
 
 endmodule
